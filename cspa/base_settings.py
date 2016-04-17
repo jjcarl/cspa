36149543a09ee76d5443d69d3416b217d34c7ecb
Jonathon Carlson <jon@jc2dev.com>
2016-04-16 23:01:37 -0600

    getting started

--- cspa/base_settings.py
+++ cspa/base_settings.py
@@ -24,6 +24,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cspa',
+    'core',
+    'scheduling',
 )
 
 MIDDLEWARE_CLASSES = (
